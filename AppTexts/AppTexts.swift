93a723b6751f7669ead563fe27bd51a3677f1feb
SalahMohamed <>
2022-10-18 23:08:50 +0300

    no message

---
+++ AppTexts/AppTexts.swift
@@ -0,0 +1,17 @@
+//
+//  AppTexts.swift
+//  AppTexts
+//
+//  Created by Salah on 11/30/21.
+//  Copyright © 2021 Salah. All rights reserved.
+//
+
+import UIKit
+
+public class AppTexts: NSObject {
+    // Constants
+    
+    public class var test22:String{
+        return "test".internalLocalize_
+    }
+}
