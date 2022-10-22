318ba5c9d28400396167b3eabbe8d0f31a98a495
SalahMohamed <>
2022-10-22 13:02:57 +0300

    no message

--- AppTexts/AppTexts.swift
+++ AppTexts/AppTexts.swift
@@ -42,8 +42,8 @@ case add="General.QuestionYesNo.Add"
 case edit="General.QuestionYesNo.Edit"
 case save="General.QuestionYesNo.Save"
 case send="General.QuestionYesNo.Send"
-case Change="General.QuestionYesNo.Change"
-case Cutome="General.QuestionYesNo"
+case change="General.QuestionYesNo.Change"
+case cutome="General.QuestionYesNo"
     public func string(_ value:String)->String{
     return String.init(format:self.rawValue.internalLocalize_, arguments:[value])
 }
@@ -55,6 +55,7 @@ case Edited="General.Edited.Successfully"
 case Saved="General.Saved.Successfully"
 case Sent="General.Sent.Successfully"
 case Change="General.Change.Successfully"
+case uploaded="General.Uploaded.Successfully"
 func string(_ value:String?)->String{
     if let value:String=value{
         return String.init(format:self.rawValue.internalLocalize_, arguments:[value])
@@ -65,13 +66,6 @@ func string(_ value:String?)->String{
 }
 public class AppTexts: NSObject {
     // Constants
-    
-    public class var Requirements:String{
-        return "Requirements".internalLocalize_
-    }
-    public class var Location:String{
-        return "Location".internalLocalize_
-    }
     public class var PasswordMustHave6:String{
         return "PasswordMustHave6".internalLocalize_
     }
@@ -135,6 +129,87 @@ public class AppTexts: NSObject {
     public class var Send:String{
         return "Send".internalLocalize_
     }
+    public class var Year:String{
+        return "Year".internalLocalize_
+    }
+    public class var Clear:String{
+        return "Clear".internalLocalize_
+    }
+    public class var Sort:String{
+        return "Sort".internalLocalize_
+    }
+    public class var SortBy:String{
+        return "SortBy".internalLocalize_
+    }
+    public class var Home:String{
+        return "Home".internalLocalize_
+    }
+    public class var SeeAll:String{
+        return "SeeAll".internalLocalize_
+    }
+    public class var Favorite:String{
+        return "Favorite".internalLocalize_
+    }
+    public class var Signin:String{
+        return "Signin".internalLocalize_
+    }
+    public class var Change:String{
+        return "Change".internalLocalize_
+    }
+    public class var Edit:String{
+        return "Edit".internalLocalize_
+    }
+    func KM(value:String)->String{
+    return String.init(format: "KM".internalLocalize_, arguments:[value])
+    }
+    public class var ShowMore:String{
+        return "ShowMore".internalLocalize_
+    }
+    public class var ShowLess:String{
+        return "ShowLess".internalLocalize_
+    }
+    public class var Filter:String{
+        return "Filter".internalLocalize_
+    }
+    public class var SearchResult:String{
+        return "SearchResult".internalLocalize_
+    }
+    public class var Add:String{
+        return "Add".internalLocalize_
+    }
+    public class var Back:String{
+        return "Back".internalLocalize_
+    }
+    public class var Activate:String{
+        return "Activate".internalLocalize_
+    }
+    public class var Next:String{
+        return "Next".internalLocalize_
+    }
+    public class var Previous:String{
+        return "Previous".internalLocalize_
+    }
+    public class var WriteAComment:String{
+        return "WriteAComment".internalLocalize_
+    }
+    public class var Attachments:String{
+        return "Attachments".internalLocalize_
+    }
+    public class var Continue:String{
+        return "Continue".internalLocalize_
+    }
+    public class var ClearAll:String{
+        return "ClearAll".internalLocalize_
+    }
+    public class var File:String{
+        return "File".internalLocalize_
+    }
+    public class var Photo:String{
+        return "Photo".internalLocalize_
+    }
+    public class var ProfilePicture:String{
+        return "ProfilePicture".internalLocalize_
+    }
     // Alerts
     public class var LogoutConfirmation:String{
         return "AlertMessageLogoutConfirmation".internalLocalize_
@@ -191,6 +266,90 @@ public class AppTexts: NSObject {
     public class var Chat:String{
         return "Chat".internalLocalize_
     }
+    public class var AccountInformation:String{
+        return "AccountInformation".internalLocalize_
+    }
+    public class var Skip:String{
+        return "Skip".internalLocalize_
+    }
+    public class var Join:String{
+        return "Join".internalLocalize_
+    }
+    public class var Delete:String{
+        return "Delete".internalLocalize_
+    }
+    public class var PaymentSuccessful:String{
+        return "PaymentSuccessful".internalLocalize_
+    }
+    public class var MyAccount:String{
+        return "MyAccount".internalLocalize_
+    }
+    public class var Pending:String{
+        return "Pending".internalLocalize_
+    }
+    public class var Confirmed:String{
+        return "Confirmed".internalLocalize_
+    }
+    public class var Cancelled:String{
+        return "Cancelled".internalLocalize_
+    }
+    public class var Unconfirmed:String{
+        return "Unconfirmed".internalLocalize_
+    }
+    public class var Confirm:String{
+        return "Confirm".internalLocalize_
+    }
+    public class var Version:String{
+        return "Version".internalLocalize_
+    }
+    public class var ForgotPassword:String{
+        return "ForgotPassword".internalLocalize_
+    }
+    public class var Male:String{
+        return "Male".internalLocalize_
+    }
+    public class var Female:String{
+        return "Female".internalLocalize_
+    }
+    public class var Other:String{
+        return "Other".internalLocalize_
+    }
+    public class var Verfiy:String{
+        return "Verfiy".internalLocalize_
+    }
+    public class var AppLanguage:String{
+        return "AppLanguage".internalLocalize_
+    }
+    public class var ShareApp:String{
+        return "ShareApp".internalLocalize_
+    }
+    public class var ChangePassword:String{
+        return "ChangePassword".internalLocalize_
+    }
+    public class var ResendVerificationCode:String{
+        return "ResendVerificationCode".internalLocalize_
+    }
+    public class var CompleteProfile:String{
+        return "CompleteProfile".internalLocalize_
+    }
+    public class var Instructions:String{
+        return "Instructions".internalLocalize_
+    }
+    public class var RateUs:String{
+        return "RateUs".internalLocalize_
+    }
+    public class var Price:String{
+        return "Price".internalLocalize_
+    }
+    public class var ExpiryDate:String{
+        return "ExpiryDate".internalLocalize_
+    }
+    public class var UsagePolicy:String{
+        return "UsagePolicy".internalLocalize_
+    }
+    public class var Verification:String{
+        return "Verification".internalLocalize_
+    }
     // Fields
     public class var FirstName:String{
         return "FirstName".internalLocalize_
@@ -246,7 +405,49 @@ public class AppTexts: NSObject {
     public class var DateOfBirth:String{
         return "DateOfBirth".internalLocalize_
     }
-    public class var Version:String{
-        return "Version".internalLocalize_
+    public class var Location:String{
+        return "Location".internalLocalize_
+    }
+    public class var Requirements:String{
+        return "Requirements".internalLocalize_
+    }
+    public class var MessageTitle:String{
+        return "MessageTitle".internalLocalize_
+    }
+    public class var MessageBody:String{
+        return "MessageBody".internalLocalize_
+    }
+    public class var VerificationCode:String{
+        return "VerificationCode".internalLocalize_
+    }
+    public class var Country:String{
+        return "Country".internalLocalize_
+    }
+    public class var ChatTextMessagePlaceholder:String{
+        return "ChatTextMessagePlaceholder".internalLocalize_
+    }
+    public class var GeneralMessage:String{
+        return "General.Message".internalLocalize_
+    }
+    public class var AddressOnMap:String{
+        return "AddressOnMap".internalLocalize_
+    }
+    public class var CurrentLocation:String{
+        return "CurrentLocation".internalLocalize_
+    }
+    public class var SearchHere:String{
+        return "SearchHere".internalLocalize_
+    }
+    public class var Nationality:String{
+        return "Nationality".internalLocalize_
+    }
+    public class var DrivingLicense:String{
+        return "DrivingLicense".internalLocalize_
+    }
+    public class var BirthCertificate:String{
+        return "BirthCertificate".internalLocalize_
+    }
+    public class var UniversityCertificate:String{
+        return "UniversityCertificate".internalLocalize_
     }
 }
