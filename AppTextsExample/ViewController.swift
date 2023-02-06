4a9d52be09a4e0d9df9e79c418481a0fcf6ecc19
SalahMohamed <>
2023-02-06 10:51:43 +0200

    no message

--- AppTextsExample/ViewController.swift
+++ AppTextsExample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(AppTexts.ConfirmPassword);
+        
         // Do any additional setup after loading the view.
     }
 
